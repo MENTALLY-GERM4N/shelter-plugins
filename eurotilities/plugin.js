7c1f2845d5511ee3d61cfe9acafa867529e8813c
wont-stream <wont-stream@users.noreply.github.com>
2024-09-22 22:03:58 +0000

    deploy: d9135cb5ce40a595f73085b26b02776c27613634

--- eurotilities/plugin.js
+++ eurotilities/plugin.js
@@ -103,6 +103,63 @@
     }
     return result;
   }
+  {
+    let handleModuleNotFound2 = function(method, ...filter) {
+      const errMsg = `webpack.${method} found no module. Filter: ${filter}`;
+      console.error(new Error(errMsg));
+      return { error: errMsg };
+    }, find2 = function(filter, { isIndirect = false, isWaitFor = false } = {}) {
+      if (typeof filter !== "function") {
+        return handleModuleNotFound2("find", "Invalid filter function");
+      }
+      for (const key in cache2) {
+        const mod = cache2[key];
+        if (!mod.loaded || !mod?.exports)
+          continue;
+        const exports = mod.exports;
+        if (filter(exports))
+          return isWaitFor ? [exports, key] : exports;
+        if (exports.default && filter(exports.default)) {
+          const found = exports.default;
+          return isWaitFor ? [found, key] : found;
+        }
+        if (typeof exports === "object") {
+          for (const nestedKey in exports) {
+            if (nestedKey.length > 3)
+              continue;
+            const nested = exports[nestedKey];
+            if (nested && filter(nested)) {
+              return isWaitFor ? [nested, key] : nested;
+            }
+          }
+        }
+      }
+      if (!isIndirect) {
+        return handleModuleNotFound2("find", filter);
+      }
+      return isWaitFor ? [null, null] : null;
+    }, findByProps2 = function(...props) {
+      const result = find2(filters2.byProps(...props), { isIndirect: true });
+      if (!result || result.error) {
+        return handleModuleNotFound2("findByProps", ...props);
+      }
+      return result;
+    };
+    let wreq2;
+    let cache2;
+    window.webpackChunkdiscord_app.push([
+      [Symbol()],
+      {},
+      (x) => {
+        wreq2 = x;
+      }
+    ]);
+    window.webpackChunkdiscord_app.pop();
+    cache2 = wreq2.c;
+    const filters2 = {
+      byProps: (...props) => (m) => props.every((p) => m[p] !== void 0)
+    };
+  }
 
   // plugins/eurotilities/modules/colorSighted.ts
   var { Masks } = findByProps("Masks");
@@ -340,7 +397,8 @@
           showToast({
             ...modules_default[module],
             title: `${modules_default[module].title} - Enabled`,
-            content: null
+            content: null,
+            duration: 0
           });
         }
       }
