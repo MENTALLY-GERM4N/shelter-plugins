388b0a399ef2277178deba0c213f4204d1b087e4
wont.stream <drns.pw@icloud.com>
2024-09-27 15:17:45 -0400

    fix

--- plugins/arRPC/index.ts
+++ plugins/arRPC/index.ts
@@ -1,12 +1,17 @@
+import { findByProps } from "./webpack.ts";
+import type { ShelterApi } from "@uwu/shelter-defs";
+
 const {
 	flux: {
 		dispatcher: FluxDispatcher,
 		stores: { ApplicationStore },
 	},
-	http: { get },
-} = shelter;
+	http: { get, post },
+} = shelter as ShelterApi;
+
 const apps = {};
 const assetCache = {};
+const httpAssetCache = {};
 
 const fetchAssetId = async (applicationId, assetName) => {
 	// TO-DO Use APPLICATION_ASSETS_UPDATE and APPLICATION_ASSETS_FETCH
@@ -35,63 +40,57 @@ const fetchAssetId = async (applicationId, assetName) => {
 	return resource ? resource.id : null;
 };
 
-export class Socket extends WebSocket {
-	private _reconnectDelay: number;
-	private _url: string;
+const getStuff = findByProps("fetchAssetIds", "getAssetImage");
+const fetchHttpAsset = async (applicationId, assetId) => {
+	if (!httpAssetCache[assetId]) {
+		try {
+			const response = await getStuff.fetchAssetIds(applicationId, [assetId]);
 
-	constructor(url: string) {
-		super(url);
-		this._url = url;
-		this._reconnectDelay = 1000;
-
-		/*
-		this.addEventListener("open", () => {
-			console.log(`Socket > Connected to ${new URL(url).hostname}`);
-		});
-		*/
-
-		this.addEventListener("close", () => {
-			setTimeout(() => {
-				this._reconnect();
-			}, this._reconnectDelay);
-		});
-
-		this.addEventListener("error", () => {
-			setTimeout(() => {
-				this._reconnect();
-			}, this._reconnectDelay);
-		});
+			httpAssetCache[assetId] = response[0];
+		} catch (error) {
+			console.error("Request failed", error);
+			return null;
+		}
 	}
 
-	private _reconnect() {
-		this.close();
-		const newSocket = new Socket(this._url);
-		Object.setPrototypeOf(this, Object.getPrototypeOf(newSocket));
-		Object.assign(this, newSocket);
-	}
-}
+	return httpAssetCache[assetId];
+};
 
 // https://images-ext-1.discordapp.net/external/1u9jSonO6pasZ41RA1LGqSbTysKHHF0MzoL0YDeTJg0/https/lh3.googleusercontent.com/fhDgRO0LrPPo9CDqsLQlxR3CVZc8xPmSi9Ja8DKAS5zhoWsZKdj2scyWqBUU2t4DHxK1xcbWKY2Q7cpj%3Dw544-h544-l90-rj?format=webp&width=300&height=300
 export function onLoad() {
-	const ws = new Socket("ws://localhost:1337");
+	const ws = new WebSocket("ws://localhost:1337");
 
 	ws.onmessage = async ({ data }) => {
 		const msg = JSON.parse(data);
 
-		if (msg.activity?.assets?.large_image.startsWith("https://")) {
-			msg.activity.assets.large_image = `https://images-ext-1.discordapp.net/external/${msg.activity.assets.large_image.replace("https://", "https/")}?format=webp&width=300&height=300`;
-		} else {
-			if (msg.activity?.assets?.large_image)
+		if (msg.activity?.assets?.large_image) {
+			if (msg.activity.assets.large_image.startsWith("https://")) {
+				msg.activity.assets.large_image = await fetchHttpAsset(
+					msg.activity.application_id,
+					msg.activity.assets.large_image,
+				);
+			} else {
 				msg.activity.assets.large_image = await fetchAssetId(
 					msg.activity.application_id,
 					msg.activity.assets.large_image,
 				);
-			if (msg.activity?.assets?.small_image)
-				msg.activity.assets.small_image = await fetchAssetId(
-					msg.activity.application_id,
-					msg.activity.assets.small_image,
-				);
+			}
+
+			if (msg.activity.assets.small_image) {
+				if (msg.activity.assets.small_image.startsWith("https://")) {
+					msg.activity.assets.small_image = await fetchHttpAsset(
+						msg.activity.application_id,
+						msg.activity.assets.small_image,
+					);
+				} else {
+					msg.activity.assets.small_image = await fetchAssetId(
+						msg.activity.application_id,
+						msg.activity.assets.small_image,
+					);
+				}
+			}
 		}
+
 		if (msg.activity) {
 			// TODO - Support games from DB too lool
 			const appId = msg.activity.application_id;
