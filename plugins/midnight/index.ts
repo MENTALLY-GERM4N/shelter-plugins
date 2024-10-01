c5d7aaf7b1f5e359c0b7b6641c2a52aec4e6c829
wont.stream <drns.pw@icloud.com>
2024-10-01 14:08:33 -0400

    update plugin name, add midnight plugin

---
+++ plugins/midnight/index.ts
@@ -0,0 +1,11 @@
+const {
+	util: { log },
+} = shelter;
+
+export function onLoad() {
+	document.body.classList.add("theme-midnight");
+}
+
+export function onUnload() {
+	document.body.classList.remove("theme-midnight");
+}
